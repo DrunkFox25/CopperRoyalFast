f18720acb053f1d96d3dc63795db48b30ee4d9f3
Peter Aravin <112434193+DrunkFox25@users.noreply.github.com>
2026-03-15 01:03:22 +0000

    more testing

---
+++ testing.js
@@ -0,0 +1,39 @@
+import {joinRoom, selfId} from 'https://esm.run/trystero';
+
+const config = {
+    appId: 'FastCopperRoyal',
+    turnConfig: [
+        {
+            urls: ['turn:openrelay.metered.ca:80', 'turn:openrelay.metered.ca:443'],
+            username: 'openrelayproject',
+            credential: 'openrelayproject'
+        }
+    ],
+    rtcConfig: {
+        iceServers: [{ urls: 'stun:stun.l.google.com:19302' }]
+    }
+};
+
+const text = document.getElementById("testing");
+
+const room = joinRoom(config, '0');
+
+text.innerHTML += `You are ${selfId}<br>`;
+
+room.onPeerJoin((peerId) => {
+    text.innerHTML += `${peerId} joined<br>`;
+});
+
+room.onPeerLeave((peerId) => {
+    text.innerHTML += `${peerId} left<br>`;
+});
+
+const [sendHello, getHello] = room.makeAction('hell');
+
+getHello((str, peerId) => {
+    text.innerHTML += `${peerId} says ${str}<br>`
+});
+
+function hello(){
+    sendHello('hi');
+}
